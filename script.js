13ea7ab3708309b59ec3e8ae5e267a746240fe52
mch-sg <magnus.hvidtfeldt@gmail.com>
2022-09-03 02:10:07 +0200

    size & license patch + assets init

--- script.js
+++ script.js
@@ -6,28 +6,6 @@ window.onscroll = function() {myFunction()};
 // Get the header
 var header = document.getElementById("myHeader");
 
-// Get the offset position of the navbar
-var stickee = header.offsetTop;
-
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset > stickee) {
-    header.classList.add("stickee");
-  } else {
-    header.classList.remove("stickee");
-  }
-}
-
-
-
-document.getElementById('dark-theme').addEventListener('click', () => {
-    document.documentElement.style.setProperty('--background-color', '#000')
-} )
-
-document.getElementById('light-theme').addEventListener('click', () => {
-    document.documentElement.style.setProperty('--background-color', '#fff')
-} ) 
-
 
 $(".flexbox-item").click(function () {
     window.open(
