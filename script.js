70ba027cc757ba18236882a8b9eb6dfda62d7197
mch-sg <magnus.hvidtfeldt@gmail.com>
2022-11-05 21:18:15 +0100

    localStorage

--- script.js
+++ script.js
@@ -22,6 +22,28 @@ $("a").click(function (event) {
 // dark mode
 function dark() {
   var element = document.body;
-  element.classList.toggle("dark-mode");
+  element.classList.toggle("dark-theme");
   console.log("test");
+
+  localStorage.setItem("dark-theme", "enabled");
 }
+
+let darkMode = localStorage.getItem("dark-theme");
+
+if (darkMode === "enabled") {
+  dark(); // set state of darkMode on page load
+}
+
+
+// scrollbar
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  if ((changeInfo.status == 'loading' || changeInfo.status == 'complete') && tab.active) {
+    // check if is reddit page
+    if (tab.url.indexOf('reddit.com') > -1) {
+      // insert css
+      const css = 'body { background-color: var(--newCommunityTheme-canvas); }';
+      chrome.scripting.insertCSS({ target: { tabId: tabId }, css: css, });
+    }
+  }
+}); 
+
